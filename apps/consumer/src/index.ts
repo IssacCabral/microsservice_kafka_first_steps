53ca8c8d930fa984d5c7e47ea5628b03e34cab4c
Issac Cabral <issaccabrall@gmail.com>
2022-08-23 15:24:34 -0300

    chore: consumer app now is running on Docker

--- apps/consumer/src/index.ts
+++ apps/consumer/src/index.ts
@@ -1,1 +1,13 @@
-console.log('dentro do consumer')
+import express from 'express'
+import env from './config/env'
+
+const app = express()
+const PORT = env.SERVER_PORT
+
+app.get('/', (req, res) => {
+    return res.send({message: 'Consumer Server'})
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
